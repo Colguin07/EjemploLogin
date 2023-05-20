c3be420e0ecde7eb97b69969565e43b05f18f4ec
Colguin07 <claudio.olguin07@gmail.com>
2023-05-20 13:58:44 -0400

    prueba 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,15 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  /*{
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },*/
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  }
 ];
 
 @NgModule({
